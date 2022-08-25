fd8753b3924d94a94aeb923fc84c54a7ee88fa47
bigbabyq <110021221+bigbabyq@users.noreply.github.com>
2022-08-25 20:20:37 +0800

    session模板2

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ class User:
     password: str
 
 
-users = [
+users1 = [
     User(1, "Admin", "123456"),
-    User(2, "Eason", "888888"),
-    User(3, "Tommy", "666666"),
+]
+users2 = [
+
+    User(1, "Eason", "888888"),
+
 ]
 
 
@@ -24,8 +27,10 @@ users = [
 def before_request():
     g.user = None
     if 'user_id' in session:
-        user = [u for u in users if u.id == session['user_id']][0]
-        g.user = user
+        user1 = [u for u in users1 if u.id == session['user_id']][0]
+        user2 = [u for u in users2 if u.id == session['user_id']][0]
+        g.user1 = user1
+        g.user2 = user2
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -35,22 +40,41 @@ def login():
         session.pop('user_id', None)
         username = request.form.get("username", None)
         password = request.form.get("password", None)
-        user = [u for u in users if u.username == username]
-        if len(user) > 0:
-            user = user[0]
-        if user and user.password == password:
+        user1 = [u for u in users1 if u.username == username]
+        if len(user1) > 0:
+            user = user1[0]
+        if user1 and user.password == password:
             session['user_id'] = user.id
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile1'))
+
+    if request.method == 'POST':
+        # 登录操作
+        session.pop('user_id', None)
+        username = request.form.get("username", None)
+        password = request.form.get("password", None)
+        user2 = [u for u in users2 if u.username == username]
+        if len(user2) > 0:
+            user = user2[0]
+        if user2 and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile2'))
 
     return render_template("login.html")
 
 
-@app.route("/profile")
-def profile():
-    if not g.user:
+@app.route("/profile1")
+def profile1():
+    if not g.user1:
         return redirect(url_for('login'))
 
-    return render_template("profile.html")
+    return render_template("1.html")
+
+@app.route("/profile2")
+def profile2():
+    if not g.user2:
+        return redirect(url_for('login'))
+
+    return render_template("2.html")
 
 
 @app.route("/logout")
